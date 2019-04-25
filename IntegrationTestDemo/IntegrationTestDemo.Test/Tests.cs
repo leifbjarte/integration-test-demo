e363a9bee6439819dc417c25201efeb7ae704ba4
Leif Bjarte Johansson <lejoh@equinor.com>
2019-04-25 13:05:18 +0200

    Added test with auth token set, fixed URI for error handler demo.

--- IntegrationTestDemo/IntegrationTestDemo.Test/Tests.cs
+++ IntegrationTestDemo/IntegrationTestDemo.Test/Tests.cs
@@ -24,7 +24,7 @@ namespace IntegrationTestDemo.Test
         {
             var client = factory.CreateClient();
 
-            var result = await client.GetAsync("api/values/1");
+            var result = await client.GetAsync("persons/test@testesen.com");
 
             result.StatusCode.Should().Be(HttpStatusCode.NotModified);
         }
@@ -34,9 +34,48 @@ namespace IntegrationTestDemo.Test
         /// </summary>
         /// <returns></returns>
         [Fact]
-        public async Task UploadFileForPersonShouldWork()
+        public async Task UploadFileAnonymouslyForPersonShouldWork()
         {
             var client = factory.CreateClient();
+
+            var message = new HttpRequestMessage(HttpMethod.Post, $"persons/test@testesen.com/documents/{Guid.NewGuid()}");
+            message.Headers.Add("x-test", "It works!");
+
+            using (var fileContent = new MultipartFormDataContent())
+            {
+                var bytes = await File.ReadAllBytesAsync("Files/TestContent.json");
+                fileContent.Add(new ByteArrayContent(bytes), "file", "TestContent.json");
+                message.Content = fileContent;
+
+                var response = await client.SendAsync(message);
+                var body = await response.Content.ReadAsStringAsync();
+
+                var deserializeAs = new
+                {
+                    Name = string.Empty,
+                    Email = string.Empty,
+                    UniqueId = Guid.Empty
+                };
+
+                var personId = JsonConvert.DeserializeAnonymousType(body, deserializeAs);
+                personId.Name.Should().Be("Test Testesen");
+                personId.Email.Should().Be("test@testesen.com");
+                personId.UniqueId.Should().NotBeEmpty();
+
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+            }
+        }
+        
+        /// <summary>
+        /// Illustrating file upload support, model binding, anonymous deseralization and custom headers
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task UploadFileWithAuthenticationForPersonShouldWork()
+        {
+            var client = factory.CreateClient()
+                .WithTestUser(TestUserProfile.TestTestesen)
+                .AddTestAuthToken();
 
             var message = new HttpRequestMessage(HttpMethod.Post, $"persons/test@testesen.com/documents/{Guid.NewGuid()}");
             message.Headers.Add("x-test", "It works!");
