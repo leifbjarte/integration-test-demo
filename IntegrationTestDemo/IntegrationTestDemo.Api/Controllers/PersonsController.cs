67d569a7f033de127c36f2c8a742e14eee02eea7
Leif Bjarte Johansson <lejoh@equinor.com>
2019-04-25 13:00:36 +0200

    Combined simple scenario into one test.

--- IntegrationTestDemo/IntegrationTestDemo.Api/Controllers/PersonsController.cs
+++ IntegrationTestDemo/IntegrationTestDemo.Api/Controllers/PersonsController.cs
@@ -1,6 +1,11 @@
-﻿using IntegrationTestDemo.Api.ModelBinding;
+﻿using IntegrationTestDemo.Api.ErrorHandling;
+using IntegrationTestDemo.Api.ModelBinding;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace IntegrationTestDemo.Api.Controllers
 {
@@ -8,7 +13,23 @@ namespace IntegrationTestDemo.Api.Controllers
     public class PersonsController : ControllerBase
     {
         [HttpGet("{personId}")]
-        [Authorize]
-        public IActionResult GetPerson(PersonId personId) => new OkObjectResult(personId);
+        public IActionResult GetPerson(PersonId personId)
+        {
+            throw new SomeCustomError();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("{personId}/documents/{documentId}")]
+        public async Task<IActionResult> UploadFile(PersonId personId, Guid documentId, IFormFile file)
+        {
+            var testHeader = Request.Headers.ContainsKey("x-test") ? Request.Headers["x-test"].FirstOrDefault() : null;
+
+            using (var fileStream = file.OpenReadStream())
+            {
+                //Upload to blob, write to disk, send to someone - or just ignore
+            }
+
+            return new OkObjectResult(personId);
+        }
     }
 }
--- IntegrationTestDemo/IntegrationTestDemo.Test/Tests.cs
+++ IntegrationTestDemo/IntegrationTestDemo.Test/Tests.cs
@@ -10,30 +10,18 @@ using Xunit;
 
 namespace IntegrationTestDemo.Test
 {
-    public class Tests
+    public class Tests : IClassFixture<IntegrationTestWebAppFactory>
     {
-        /// <summary>
-        /// Test to illustrate basic use of web app factory, including sending headers
-        /// </summary>
-        /// <returns></returns>
-        [Fact]
-        public async Task GetValuesShouldBeSuccessfull()
+        private readonly IntegrationTestWebAppFactory factory;
+
+        public Tests(IntegrationTestWebAppFactory factory)
         {
-            var factory = new IntegrationTestWebAppFactory();
-            var client = factory.CreateClient();
-
-            var message = new HttpRequestMessage(HttpMethod.Get, "api/values");
-            message.Headers.Add("x-test", "It works!");
-
-            var result = await client.SendAsync(message);
-
-            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            this.factory = factory;
         }
 
         [Fact]
         public async Task GetValueByIdShouldThrowException()
         {
-            var factory = new IntegrationTestWebAppFactory();
             var client = factory.CreateClient();
 
             var result = await client.GetAsync("api/values/1");
@@ -42,51 +30,38 @@ namespace IntegrationTestDemo.Test
         }
 
         /// <summary>
-        /// Illustrating model binder support + anonymous types to create completely decoupled, refactor-safe tests.
+        /// Illustrating file upload support, model binding, anonymous deseralization and custom headers
         /// </summary>
         /// <returns></returns>
         [Fact]
-        public async Task GetPersonShouldInvokeModelBinder()
+        public async Task UploadFileForPersonShouldWork()
         {
-            var factory = new IntegrationTestWebAppFactory();
             var client = factory.CreateClient();
 
-            var result = await client.GetAsync("persons/test@testesen.com");
-            var body = await result.Content.ReadAsStringAsync();
-
-            var deserializeAs = new
-            {
-                Name = string.Empty,
-                Email = string.Empty,
-                UniqueId = Guid.Empty
-            };
-
-            var personId = JsonConvert.DeserializeAnonymousType(body, deserializeAs);
-            personId.Name.Should().Be("Test Testesen");
-            personId.Email.Should().Be("test@testesen.com");
-            personId.UniqueId.Should().NotBeEmpty();
-
-            result.StatusCode.Should().Be(HttpStatusCode.OK);
-        }
-
-        /// <summary>
-        /// Illustrating file upload support
-        /// </summary>
-        /// <returns></returns>
-        [Fact]
-        public async Task UploadFileShouldNotFail()
-        {
-            var factory = new IntegrationTestWebAppFactory();
-            var client = factory.CreateClient()
-                .WithTestUser(TestUserProfile.TestTestesen)
-                .AddTestAuthToken();
+            var message = new HttpRequestMessage(HttpMethod.Post, $"persons/test@testesen.com/documents/{Guid.NewGuid()}");
+            message.Headers.Add("x-test", "It works!");
 
             using (var fileContent = new MultipartFormDataContent())
             {
                 var bytes = await File.ReadAllBytesAsync("Files/TestContent.json");
                 fileContent.Add(new ByteArrayContent(bytes), "file", "TestContent.json");
+                message.Content = fileContent;
 
-                var response = await client.PostAsync($"files/{Guid.NewGuid()}", fileContent);
+                var response = await client.SendAsync(message);
+                var body = await response.Content.ReadAsStringAsync();
+
+                var deserializeAs = new
+                {
+                    Name = string.Empty,
+                    Email = string.Empty,
+                    UniqueId = Guid.Empty
+                };
+
+                var personId = JsonConvert.DeserializeAnonymousType(body, deserializeAs);
+                personId.Name.Should().Be("Test Testesen");
+                personId.Email.Should().Be("test@testesen.com");
+                personId.UniqueId.Should().NotBeEmpty();
+
                 response.StatusCode.Should().Be(HttpStatusCode.OK);
             }
         }
